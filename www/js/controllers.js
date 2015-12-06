f41467be028ecde72c1dc2e34f93a6798ba7de1e
Nick Ajderian <nickajderian@hotmail.com>
2015-12-06 09:00:52 +0000

    stage 1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,30 +1,55 @@
 angular.module('app.controllers', [])
-  
+
 .controller('myReportSubmissionCtrl', function($scope) {
 
 })
-   
-.controller('loginCtrl', function($scope) {
+
+.controller('loginCtrl', function($scope, ModelSvc, $state) {
+  $scope.model = ModelSvc;
+  $scope.onLogin = function(){
+    if($scope.model.username === "nick" && $scope.model.password==="nick"){
+      //ok we are good to go to the next page
+      alert('Hello ' + $scope.model.username + ' and welcome to My Report App');
+      $scope.model.newReport = {
+        reportName:'new report'
+      };
+      $state.go("newReport");
+    }else{
+      alert("Incorrect user name or password");
+    }
+  }
 
 })
-   
-.controller('newReportCtrl', function($scope) {
+
+.controller('newReportCtrl', function($scope,ModelSvc, $state) {
+  $scope.model = ModelSvc;
+  $scope.onNext = function(){
+    var msg = JSON.stringify($scope.model.newReport);
+    alert(msg);
+    $state.go("checklist"); //go to the NAME of the state
+    //when you use href="xx" in the html this goes to the URL
+  }
+  //$scope.model.newReport.reportName === 'new report' I hope!
+})
+
+.controller('checklistCtrl', function($scope,ModelSvc, $state) {
+  $scope.model = ModelSvc;
+  $scope.onNext = function(){
+    var msg = JSON.stringify($scope.model.newReport);
+    alert(msg);
+    $state.go("summary");
+  };
 
 })
-   
-.controller('checklistCtrl', function($scope) {
 
-})
-   
 .controller('summaryCtrl', function($scope) {
 
 })
-   
+
 .controller('alertCtrl', function($scope) {
 
 })
-   
+
 .controller('thanksCtrl', function($scope) {
 
 })
- 
