e1489edf3c1d8a86594883adafefec1465acd908
Nick Ajderian <nickajderian@hotmail.com>
2015-12-06 10:41:14 +0000

    SummaryPage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,7 +9,7 @@ angular.module('app.controllers', [])
   $scope.onLogin = function(){
     if($scope.model.username === "nick" && $scope.model.password==="nick"){
       //ok we are good to go to the next page
-      alert('Hello ' + $scope.model.username + ' and welcome to My Report App');
+      //alert('Hello ' + $scope.model.username + ' and welcome to My Report App');
       $scope.model.newReport = {
         reportName:'new report'
       };
@@ -25,8 +25,9 @@ angular.module('app.controllers', [])
   $scope.model = ModelSvc;
   $scope.onNext = function(){
     var msg = JSON.stringify($scope.model.newReport);
-    alert(msg);
-    $state.go("checklist"); //go to the NAME of the state
+    //alert(msg);
+    $state.go("summary");
+    //SKIP CHECKLIST FOR NOW $state.go("checklist"); //go to the NAME of the state
     //when you use href="xx" in the html this goes to the URL
   }
   //$scope.model.newReport.reportName === 'new report' I hope!
@@ -42,7 +43,24 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('summaryCtrl', function($scope) {
+.controller('summaryCtrl', function($scope, ModelSvc, $state) {
+  $scope.model = ModelSvc;
+  $scope.items = [];
+  $scope.items.push({key:'Report Name', value:$scope.model.newReport.reportName});
+  $scope.items.push({key:'Staff Name', value:$scope.model.newReport.staffName});
+  $scope.items.push({key:'Date Submitted', value:$scope.model.newReport.dateSubmitted});
+  $scope.items.push({key:'Comments', value:$scope.model.newReport.comments});
+  $scope.items.push({key:'Quality Checks', value:$scope.model.newReport.qualityChecksOk ? 'OK' : 'Not done'});
+  $scope.items.push({key:'Printable', value:$scope.model.newReport.printableOk ? 'PRINTABLE': 'Not done'});
+  $scope.items.push({key:'Security Checks', value:$scope.model.newReport.securityOk ? 'OK': 'Not done'});
+  $scope.items.push({key:'Sign Off', value:$scope.model.newReport.signOffNeeded ? 'Required': 'Not required'});
+
+  //signOffNeeded
+  $scope.onNext = function(){
+    var msg = JSON.stringify($scope.model.newReport);
+    alert(msg);
+    $state.go("thanks");
+  }
 
 })
 
